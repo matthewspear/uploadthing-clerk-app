d5bca1f77de31537fe7ffd97097136f84e8ad73e
Matt Spear <matthewtimothyspear@gmail.com>
2023-06-27 14:18:25 +0800

    Follow Uploadthing getting started guide
    https://docs.uploadthing.com/nextjs/pagedir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { api } from "@/utils/api";
+import "@uploadthing/react/styles.css";
 import "@/styles/globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
---
+++ src/pages/example-uploader.tsx
@@ -0,0 +1,20 @@
+import { UploadButton } from "@/utils/uploadthing";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res: any) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </main>
+  );
+}
