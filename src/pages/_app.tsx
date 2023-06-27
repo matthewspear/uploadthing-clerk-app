ef443e95566db37c5519ebae1d7b5beede5008ca
Matt Spear <matthewtimothyspear@gmail.com>
2023-06-27 14:01:40 +0800

    Follow Clerk getting started guide
    https://clerk.com/docs/nextjs/get-started-with-nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
 
 export default api.withTRPC(MyApp);
