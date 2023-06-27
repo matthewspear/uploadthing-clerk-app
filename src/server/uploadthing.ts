16b951cbf59df887a7285bd8aedca867db7ef5c1
Matt Spear <matthewtimothyspear@gmail.com>
2023-06-27 14:44:17 +0800

    Add Clerk getAuth to Uploadthing router

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,20 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
 import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
 
-const f = createUploadthing();
+import { getAuth } from "@clerk/nextjs/server";
 
-const auth = (req: NextApiRequest, res: NextApiResponse) => ({ id: "fakeId" }); // Fake auth function
+const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
+    // eslint-disable-next-line
     .middleware(async ({ req, res }) => {
       // This code runs on your server before upload
-      // eslint-disable-next-line
-      const user = await auth(req, res);
+      const { user } = getAuth(req);
+      console.log("user", user);
 
       // If you throw, the user will not be able to upload
       if (!user) throw new Error("Unauthorized");
